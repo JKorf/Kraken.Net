df315fcdb1e630705256be1ba495b71bfee82e91
Jan Korf <jankorf91@gmail.com>
2022-05-01 15:28:28 +0200

    Added seconds representation to KlineInterval enum

--- Kraken.Net/Enums/KlineInterval.cs
+++ Kraken.Net/Enums/KlineInterval.cs
@@ -1,45 +1,45 @@
 ﻿namespace Kraken.Net.Enums
 {
     /// <summary>
-    /// The time interval of kline data
+    /// The time interval of kline data, the int value respresents the time in seconds
     /// </summary>
     public enum KlineInterval
     {
         /// <summary>
         /// 1m
         /// </summary>
-        OneMinute,
+        OneMinute = 60,
         /// <summary>
         /// 5m
         /// </summary>
-        FiveMinutes,
+        FiveMinutes = 60 * 5,
         /// <summary>
         /// 15m
         /// </summary>
-        FifteenMinutes,
+        FifteenMinutes = 60 * 15,
         /// <summary>
         /// 30m
         /// </summary>
-        ThirtyMinutes,
+        ThirtyMinutes = 60 * 30,
         /// <summary>
         /// 1h
         /// </summary>
-        OneHour,
+        OneHour = 60 * 60,
         /// <summary>
         /// 4h
         /// </summary>
-        FourHour,
+        FourHour = 60 * 60 * 4,
         /// <summary>
         /// 1d
         /// </summary>
-        OneDay,
+        OneDay = 60 * 60 * 24,
         /// <summary>
         /// 1w
         /// </summary>
-        OneWeek,
+        OneWeek = 60 * 60 * 7,
         /// <summary>
         /// 15d
         /// </summary>
-        FifteenDays
+        FifteenDays = 60 * 60 * 15
     }
 }
