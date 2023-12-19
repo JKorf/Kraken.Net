a52cf541134e456c9e9e3f6f0c008a346b457dd8
Michiel Post <michiel@michielpost.nl>
2023-12-19 11:11:37 +0100

    Added WithdrawMethods and WithdrawAddresses endpoints

---
+++ Kraken.Net/Objects/Models/KrakenWithdrawMethod.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace Kraken.Net.Objects.Models
+{
+    /// <summary>
+    /// Info about a withdraw method
+    /// </summary>
+    public class KrakenWithdrawMethod
+    {
+        /// <summary>
+        /// Name of the asset
+        /// </summary>
+        public string Asset { get; set; } = string.Empty;
+        /// <summary>
+        /// Name of the method
+        /// </summary>
+        public string Method { get; set; } = string.Empty;
+        /// <summary>
+        /// Name of the Network
+        /// </summary>
+        public string Network { get; set; } = string.Empty;
+        /// <summary>
+        /// Minimum amount
+        /// </summary>
+        public decimal Minimum { get; set; }
+    }
+}
