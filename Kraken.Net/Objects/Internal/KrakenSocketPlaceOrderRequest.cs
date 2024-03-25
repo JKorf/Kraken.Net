3843bcd4359bf218fbd185b6c3391a163ac07138
JKorf <jankorf91@gmail.com>
2024-03-25 10:37:12 +0100

    Fixed parameter serialization for socket client SpotApi.PlaceOrderAsync

--- Kraken.Net/Objects/Internal/KrakenSocketPlaceOrderRequest.cs
+++ Kraken.Net/Objects/Internal/KrakenSocketPlaceOrderRequest.cs
@@ -17,32 +17,32 @@ namespace Kraken.Net.Objects.Internal
         [JsonProperty("type")]
         [JsonConverter(typeof(OrderSideConverter))]
         public OrderSide Type { get; set; }
-        [JsonProperty("leverage")]
+        [JsonProperty("leverage", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string? Leverage { get; set; }
-        [JsonProperty("volume")]
+        [JsonProperty("volume", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string? Volume { get; set; }
-        [JsonProperty("userref")]
+        [JsonProperty("userref", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string? ClientOrderId { get; set; }
-        [JsonProperty("price")]
+        [JsonProperty("price", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string? Price { get; set; }
-        [JsonProperty("price2")]
+        [JsonProperty("price2", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string? SecondaryPrice { get; set; }
-        [JsonProperty("starttm")]
+        [JsonProperty("starttm", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string? StartTime { get; set; }
-        [JsonProperty("expiretm")]
+        [JsonProperty("expiretm", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string? ExpireTime { get; set; }
-        [JsonProperty("validate")]
+        [JsonProperty("validate", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public bool? ValidateOnly { get; set; }
-        [JsonProperty("close[ordertype]")]
+        [JsonProperty("close[ordertype]", DefaultValueHandling = DefaultValueHandling.Ignore)]
         [JsonConverter(typeof(OrderTypeConverter))]
         public OrderType? CloseOrderType { get; set; }
-        [JsonProperty("close[price]")]
+        [JsonProperty("close[price]", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string? ClosePrice { get; set; }
-        [JsonProperty("close[price2]")]
+        [JsonProperty("close[price2]", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string? SecondaryClosePrice { get; set; }
-        [JsonProperty("oflags")]
+        [JsonProperty("oflags", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string? Flags { get; set; }
-        [JsonProperty("reduce_only")]
+        [JsonProperty("reduce_only", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public bool? ReduceOnly { get; set; }
     }
 }
