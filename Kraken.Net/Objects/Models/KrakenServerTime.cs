73068ab4ce879a097e38e29aa3bf038bb7c54c7c
Jkorf <jankorf91@gmail.com>
2025-11-20 14:13:50 +0100

    Fixed restClient.SpotApi.ExchangeData.GetServerTimeAsync deserialization

--- Kraken.Net/Objects/Models/KrakenServerTime.cs
+++ Kraken.Net/Objects/Models/KrakenServerTime.cs
@@ -5,7 +5,7 @@ namespace Kraken.Net.Objects.Models
     internal record KrakenServerTime
     {
         [JsonConverter(typeof(DateTimeConverter))]
-        [JsonPropertyName("unixTime")]
+        [JsonPropertyName("unixtime")]
         public DateTime UnixTime { get; set; }
         [JsonPropertyName("rfc1123")]
         public string RfcTime { get; set; } = string.Empty;
