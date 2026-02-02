3aeb61b7322213de9f50d7a1059cdebcdfd5df11
Jkorf <jankorf91@gmail.com>
2026-02-02 09:40:13 +0100

    Added check for disposed client in UserClientProvider

--- Kraken.Net/Clients/KrakenUserClientProvider.cs
+++ Kraken.Net/Clients/KrakenUserClientProvider.cs
@@ -60,7 +60,7 @@ namespace Kraken.Net.Clients
         /// <inheritdoc />
         public IKrakenRestClient GetRestClient(string userIdentifier, ApiCredentials? credentials = null, KrakenEnvironment? environment = null)
         {
-            if (!_restClients.TryGetValue(userIdentifier, out var client))
+            if (!_restClients.TryGetValue(userIdentifier, out var client) || client.Disposed)
                 client = CreateRestClient(userIdentifier, credentials, environment);
 
             return client;
@@ -69,7 +69,7 @@ namespace Kraken.Net.Clients
         /// <inheritdoc />
         public IKrakenSocketClient GetSocketClient(string userIdentifier, ApiCredentials? credentials = null, KrakenEnvironment? environment = null)
         {
-            if (!_socketClients.TryGetValue(userIdentifier, out var client))
+            if (!_socketClients.TryGetValue(userIdentifier, out var client) || client.Disposed)
                 client = CreateSocketClient(userIdentifier, credentials, environment);
 
             return client;
