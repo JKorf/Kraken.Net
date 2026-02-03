a9b91e96218f0fb1b392d7afbf763e7d325bc991
JKorf <jankorf91@gmail.com>
2026-02-03 21:08:34 +0100

    wip

--- Kraken.Net/Clients/FuturesApi/KrakenRestClientFuturesApiShared.cs
+++ Kraken.Net/Clients/FuturesApi/KrakenRestClientFuturesApiShared.cs
@@ -232,7 +232,7 @@ namespace Kraken.Net.Clients.FuturesApi
             return response;
         }
 
-        public async Task<ExchangeResult<SharedSymbol[]>> GetFuturesSymbolsForBaseAssetAsync(string baseAsset)
+        async Task<ExchangeResult<SharedSymbol[]>> IFuturesSymbolRestClient.GetFuturesSymbolsForBaseAssetAsync(string baseAsset)
         {
             if (!ExchangeSymbolCache.HasCached(_topicId))
             {
@@ -244,7 +244,7 @@ namespace Kraken.Net.Clients.FuturesApi
             return new ExchangeResult<SharedSymbol[]>(Exchange, ExchangeSymbolCache.GetSymbolsForBaseAsset(_topicId, baseAsset));
         }
 
-        public async Task<ExchangeResult<bool>> SupportsFuturesSymbolAsync(SharedSymbol symbol)
+        async Task<ExchangeResult<bool>> IFuturesSymbolRestClient.SupportsFuturesSymbolAsync(SharedSymbol symbol)
         {
             if (symbol.TradingMode == TradingMode.Spot)
                 throw new ArgumentException(nameof(symbol), "Spot symbols not allowed");
@@ -259,7 +259,7 @@ namespace Kraken.Net.Clients.FuturesApi
             return new ExchangeResult<bool>(Exchange, ExchangeSymbolCache.SupportsSymbol(_topicId, symbol));
         }
 
-        public async Task<ExchangeResult<bool>> SupportsFuturesSymbolAsync(string symbolName)
+        async Task<ExchangeResult<bool>> IFuturesSymbolRestClient.SupportsFuturesSymbolAsync(string symbolName)
         {
             if (!ExchangeSymbolCache.HasCached(_topicId))
             {
--- Kraken.Net/Clients/SpotApi/KrakenRestClientSpotApiShared.cs
+++ Kraken.Net/Clients/SpotApi/KrakenRestClientSpotApiShared.cs
@@ -121,7 +121,7 @@ namespace Kraken.Net.Clients.SpotApi
             return (KrakenExchange.AssetAliases.ExchangeToCommonName(split[0]), KrakenExchange.AssetAliases.ExchangeToCommonName(split[1]));
         }
 
-        public async Task<ExchangeResult<SharedSymbol[]>> GetSpotSymbolsForBaseAssetAsync(string baseAsset)
+        async Task<ExchangeResult<SharedSymbol[]>> ISpotSymbolRestClient.GetSpotSymbolsForBaseAssetAsync(string baseAsset)
         {
             if (!ExchangeSymbolCache.HasCached(_topicId))
             {
@@ -133,7 +133,7 @@ namespace Kraken.Net.Clients.SpotApi
             return new ExchangeResult<SharedSymbol[]>(Exchange, ExchangeSymbolCache.GetSymbolsForBaseAsset(_topicId, baseAsset));
         }
 
-        public async Task<ExchangeResult<bool>> SupportsSpotSymbolAsync(SharedSymbol symbol)
+        async Task<ExchangeResult<bool>> ISpotSymbolRestClient.SupportsSpotSymbolAsync(SharedSymbol symbol)
         {
             if (symbol.TradingMode != TradingMode.Spot)
                 throw new ArgumentException(nameof(symbol), "Only Spot symbols allowed");
@@ -148,7 +148,7 @@ namespace Kraken.Net.Clients.SpotApi
             return new ExchangeResult<bool>(Exchange, ExchangeSymbolCache.SupportsSymbol(_topicId, symbol));
         }
 
-        public async Task<ExchangeResult<bool>> SupportsSpotSymbolAsync(string symbolName)
+        async Task<ExchangeResult<bool>> ISpotSymbolRestClient.SupportsSpotSymbolAsync(string symbolName)
         {
             if (!ExchangeSymbolCache.HasCached(_topicId))
             {
